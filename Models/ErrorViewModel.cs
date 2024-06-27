c42671736bfde4398c74d611e9d582afeb732089
Ruan Blotta <ruanblotta@gmail.com>
2024-06-27 16:52:28 -0300

    Teste

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace cadastroEletronicos.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
