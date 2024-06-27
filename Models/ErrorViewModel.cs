df5dee985a0e3ec394586aa0fc8aac3e31781f95
Ruan Blotta <ruanblotta@gmail.com>
2024-06-27 17:01:20 -0300

    Reestruturando o projeto em monolito/mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cadastroEletronicos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
